824c736e1377fc70f9db4c726986381958de281c
WSudul <wojciech.sudul@gmail.com>
2017-05-07 20:18:01 +0200

    reworked MazeBase to non template version (container will be stored in derived class) for using polymorphism.

--- maze_generator/MazeBase.h
+++ maze_generator/MazeBase.h
@@ -14,62 +14,53 @@ enum class MazeType
 
 
 /*!
-	Abstract Base class for maze generating
+	Abstract Base class for maze generating.
+	Does NOT care what kind of container is used.
+	at the very least generate(), clear() needs to be implemented 
 
 */
-template<typename T>
 class MazeBase
 {
 public:
 
 	MazeBase();
 	virtual ~MazeBase();
-	virtual void generate() = 0;
+	virtual void generate();
 
 	/*!
 		clear the object by resetting all data and deleting object pointed by container
 	*/
-	virtual void clear()
-	{
-		container.reset();
-		WallsNo = PathTIlesNo = 0;
-		generated = false;
-	};
+	virtual void clear();
 	
 	/*!
-		return size of maze in bytes
+		return size of maze as number of all created tiles
 	*/
-	std::size_t sizeOfMaze()=0;
+	virtual std::size_t sizeOfMaze();
+
 
 	/*!
 		return total number of walls in maze
 	*/
-	virtual std::size_t getWallsNo()
-	{
-		return WallsNo;
-	};
+	virtual std::size_t getWallsNo();
 
 	/*!
 		return total number of path tiles in maze
 	*/
-	virtual std::size_t getPathTilesNo() 
-	{
-		return PathTIlesNo;
-	};
+	virtual std::size_t getPathTilesNo();
 
-	virtual MazeType getMazeType()
-	{
-		return mazeType;
-	};
+	/*!
+		return type of maze as MazeType enum
+	*/
+	virtual MazeType getMazeType();
 
-	bool isGenerated()
-	{
-		return generated;
-	}
+	/*!
+		returns true if generate() was run and clear() was no called afterwards
+	*/
+	bool isGenerated();
 
 protected:
 
-	
+	virtual void clearBase();
 
 	std::size_t WallsNo; //number of walls in maze
 	std::size_t PathTIlesNo; //total number of path tiles in maze
@@ -78,22 +69,73 @@ protected:
 
 	bool generated; //whether a maze has been generated
 
-	std::unique_ptr<T> container; //smart pointer to container storing generated maze
+	
 
 };
 
-template<typename T>
-inline MazeBase<T>::MazeBase():
+
+inline MazeBase::MazeBase():
 WallsNo(0),
 PathTIlesNo(0),
-generated(false),
-container(nullptr)
+generated(false)
 {
 
 }
 
-template<typename T>
-inline MazeBase<T>::~MazeBase()
+
+inline MazeBase::~MazeBase()
 {
+}
+
+inline void MazeBase::generate()
+{
+	
+}
+
+/*!
+clear the object by resetting all data and deleting object pointed by container
+*/
+inline void MazeBase::clear()
+{
+	clearBase();
+}
+
+inline std::size_t MazeBase::sizeOfMaze()
+{
+	return 0;
+}
+
+/*!
+return total number of walls in maze
+*/
+inline std::size_t MazeBase::getWallsNo()
+{
+	return WallsNo;
+}
+
+/*!
+return total number of path tiles in maze
+*/
+inline std::size_t MazeBase::getPathTilesNo()
+{
+	return PathTIlesNo;
+}
+
+inline MazeType MazeBase::getMazeType()
+{
+	return mazeType;
+}
+
+inline bool MazeBase::isGenerated()
+{
+	return generated;
+}
+
+
+inline void MazeBase::clearBase()
+{
+	
+	WallsNo = PathTIlesNo = 0;
+	generated = false;
 }
 
